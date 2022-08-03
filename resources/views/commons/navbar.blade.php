4a9712f1593518389fbf2c1ff8803fd820ef8065
shinjirou.iwata <sin2021@outlook.jp>
2022-08-03 01:45:43 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,7 +1,7 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         {{-- トップページへのリンク --}}
-        <a class="navbar-brand" href="/">Microposts</a>
+        <a class="navbar-brand" href="/">予約システム</a>
 
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
@@ -11,9 +11,9 @@
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                <li class="nav-item">{!! link_to_route('signup.get', '医院登録', [], ['class' => 'nav-link']) !!}</li>
                 {{-- ログインページへのリンク --}}
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">ログイン</a></li>
             </ul>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,11 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>clinicReservations</h1>
+            <h1>予約システム</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', '医院登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            {{-- ログインへのリンク --}}
+            <a href="#" class="btn btn-lg btn-primary">ログイン</a>
         </div>
     </div>
 @endsection
